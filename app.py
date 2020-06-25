9a92e097e3ad6566f71af8c7a8f4907666d6750f
anaconda77 <62774721+anaconda77@users.noreply.github.com>
2020-06-25 23:00:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-import re, requests, sys, nltk
-from bs4 import BeautifulSoup
-from flask import Flask, render_template
-from elasticsearch import Elasticsearch
+import sys
+import re
+import requests
+import math
+import nltk
 nltk.download('stopwords')
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, redirect, url_for, session
+import pandas as pd
+import numpy as np
+from math import log
+from elasticsearch import Elasticsearch
+
 from nltk.corpus import stopwords
 
 es_host="127.0.0.1"
@@ -47,7 +55,76 @@ def crawling(url,id_):
 	res = es.index(index='data', doc_type='word', id=id_, body=e)
 	print(res)
 
+def compute_tf(list1, count):
+	tf_d = []
+	i = 0
+	for wordcnt in list1:
+		tf_d[i] = wordcnt / count
+		i+=1
+
+	return tf_d
+
+def compute_idf(n):
+
+	bow = set()
+	idf_d = {}
+	
+	es = Elasticsearch([{'host':es_host, 'port':es_port}],
+timeout = 30)
+
+	for i in range(1,n):
+		res = es.index(index='data', doc_type='word', id=i)
+		for word in res:
+			bow.add(word)
+
+	j = 0
+	for t in bow:
+		cnt = 0
+		for i in range(1,n):
+			res = es.index(index='data', doc_type='word', id=i)
+			if t in res:
+				cnt+=1
+		idf_d[j] = log(n/cnt+1)
+		j+=1
+
+	return idf_d
+
+def compute_tfidf(list1,count,n):
+	return compute_tf(list1,count)*compute_idf(n)
+
+
+def cosine_similarity(listA,listB):
+	u = []
+	v = []
+	valu = 0
+	valv = 0
+	bow = set()
+
+	for i in range(1,n):
+		res = es.index(index='data', doc_type='word', id=i)
+		for word in res:
+			bow.add(word)
+
+	for w in bow:
+		for t in listA:
+			if t==w:
+				valu+=1
+		u.append(valu)
+	
+	for w in bow:
+		for t in listB:
+			if t==w:
+				valv+=1
+		v.append(valv)
+
+	dotpro = np.dot(u,v)
+	return dotpro / norm(u)*norm(v)
+
+
+
 if __name__ == '__main__':
-	url = "http://impala.apache.org/"
-	id_ = 0
-	crawling(url,id_)
+		url = "http://impala.apache.org/"
+		id_ = 0
+		crawling(url,id_)
+		res = es.index(index='data', doc_type='word', id=0)
+		print(res)
