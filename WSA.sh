ae933e79c1138c245ca4e14c2550cc465d92b564
BREG001 <ws4368@naver.com>
2020-06-27 15:07:43 +0900

    sh file edited

--- WSA.sh
+++ WSA.sh
@@ -4,6 +4,11 @@ echo "create templates directory..."
 mkdir ./templates
 echo "copy files to temp directory..."
 cp ./home.html ./templates
+cp ./analysis.html ./templates
+cp ./tfidf.html ./templates
+cp ./cossim.html ./templates
+echo "download files..."
+pip3 install nltk
 
 echo "run Flask..."
 flask run
